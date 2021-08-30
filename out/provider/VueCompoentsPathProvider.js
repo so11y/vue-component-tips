28046f75c3fac9d920ab90c209b4cf2808d19612
2470761262 <247076126@qq.com>
2021-08-30 20:51:41 +0800

    2

--- out/provider/VueCompoentsPathProvider.js
+++ out/provider/VueCompoentsPathProvider.js
@@ -6,9 +6,27 @@ class VueCompoentsPathProvider {
         this.compoentsMap = compoentsMap;
     }
     provideDefinition(document, position, token) {
-        const word = document.getText(document.getWordRangeAtPosition(position));
+        //  const word = document.getText(document.getWordRangeAtPosition(position));
+        const line = document.lineAt(position.line);
+        const textSplite = [' ', '<', '>', '"', '\'', '.', '\\', "=", ":", "@", "(", ")", "[", "]", "{", "}", ",", "!"];
+        // 通过前后字符串拼接成选择文本
+        let posIndex = position.character;
+        let textMeta = line.text.substr(posIndex, 1);
+        let selectText = '';
+        // 前向获取符合要求的字符串
+        while (textSplite.indexOf(textMeta) === -1 && posIndex <= line.text.length) {
+            selectText += textMeta;
+            textMeta = line.text.substr(++posIndex, 1);
+        }
+        // 往后获取符合要求的字符串
+        posIndex = position.character - 1;
+        textMeta = line.text.substr(posIndex, 1);
+        while (textSplite.indexOf(textMeta) === -1 && posIndex > 0) {
+            selectText = textMeta + selectText;
+            textMeta = line.text.substr(--posIndex, 1);
+        }
         return Array.from(this.compoentsMap.keys())
-            .filter(v => v.includes(word))
+            .filter(v => v.includes(selectText))
             .map(v => new vscode.Location(vscode.Uri.file(this.compoentsMap.get(v)), new vscode.Position(0, 0)));
     }
 }
