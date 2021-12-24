c1991cc3253bfed02782ee160243f1dfd5de3516
sorry <247076126@qq.com>
2021-12-25 07:51:03 +0800

    Delete out directory

--- out/util/const.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.vscodeStoreKey = void 0;
-exports.vscodeStoreKey = "zrrz.uniSaas";
-//# sourceMappingURL=const.js.map
--- out/util/types.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.isUndefined = void 0;
-const isUndefined = (opt) => {
-    return opt === undefined;
-};
-exports.isUndefined = isUndefined;
-//# sourceMappingURL=types.js.map
